5ab00026cc303a390050ec8d5ab91651bd1d467c
moussa flo <moussa flo@moussaflo-PC>
2017-03-27 17:28:45 +0200

    Mon premier programme

---
+++ src/mon_tpe/Mon_TPE.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package mon_tpe;
+
+/**
+ *
+ * @author moussa flo
+ */
+public class Mon_TPE {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
